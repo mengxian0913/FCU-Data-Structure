6edf46898889e5181208b3d08ae5825872fee635
mengxain0913 <mengxian0913@gmail.com>
2023-11-04 16:41:53 +0800

    update

--- week07/code/a.cpp
+++ week07/code/a.cpp
@@ -1,76 +1,79 @@
-/*********************************************************
-# File Name: a.cpp
-# Author: Vincent
-# Mail: mengxian0913@gmail.com
-# Created Time: 五 11/ 3 13:19:35 2023
-*********************************************************/
-
-#pragma GCC optimize("O3")
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
 #define int long long
-#define ff first
-#define ss second
-#define fastIO cin.tie(nullptr)->sync_with_stdio(false);
-#define INF 0x7FFFFFFF
 #define pb push_back
-#define all(aa) aa.begin(),aa.end()
-#define MOD (int)(1e9+7)
+// e s
+// eeeeabcsdes
+// abcds
+stack<char> st;
 
-#define MAXN 1010
+// eaaebbseass
 
-struct MyStack {
-  int top;
-  char a[MAXN];
-  bool IsEmpty();
-  bool IsFull();
-  void Push(char);
-  char Pop();
-  MyStack() {
-    top = -1;
-  }
-};
 
-bool MyStack::IsEmpty() {
-  return (top == -1);
+string get_ans(string ms) {
+	cout << "ms: " << ms << "\n"; 
+	
+	int cnt = 0;
+	for(int i = 0; i < ms.size(); i++) {
+		if(ms[i] == 's') {
+			if(cnt > 0) {
+				while(!st.empty()) {
+					char now = st.top();
+					st.pop();
+					if(now == 'e') {
+						cnt--;
+						break;
+					}
+				}
+			}
+			
+			else {
+				st.push(ms[i]);
+			}
+		}
+	
+		else {
+			if(ms[i] == 'e') cnt++;
+			st.push(ms[i]);
+		}
+	}
+	
+	string ans = "";
+	
+	while(!st.empty()) {
+		ans = st.top() + ans;
+		st.pop(); 
+	}
+	
+	return ans;
 }
 
-bool MyStack::IsFull() {
-  return (top == MAXN - 1);
+void solve() {	
+	string ss, ms = "";
+
+	ifstream ifs("input.txt", ifstream::in);
+	
+	if(!ifs.is_open()) {
+		cout << "error\n";
+		return;
+	}
+	
+	while(getline(ifs, ss)) {
+		if(ss == "") {
+			cout << get_ans(ms) << "\n";
+			ms = "";
+		}
+		ms += ss;
+		ms += "\n";
+	}
+	
+	cout << get_ans(ms) << "\n";
+	
+	return;
 }
 
-void MyStack::Push(char ch) {
-  if(IsFull()) {
-    cout << "The Stack is Full!\n";
-    return;
-  }
-  a[++top] = ch;
-  return;
-}
-
-char MyStack::Pop() {
-  if(IsEmpty()){
-    cout << "The Stack is Empty!\n";
-    return ' ';
-  }
-  return a[top--];
-}
-
-
-void solve(){
-  MyStack S;
-  string ss;
-  while(cin >> ss) {
-    for(int i = 0; i < ss.size(); i++) {
-            
-    }
-  }
-
-  return;
-}
-
-signed main(){
-  fastIO
-  solve();
-  return 0;
+signed main() {
+	solve();
+	return 0;
 }
